ce48a420fb92ddeda340f00735437052619a9247
Anty <79324714+Anty-Anty@users.noreply.github.com>
2026-06-03 23:05:42 +0000

    feat(StarRating): add color, size, defaultRating props

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import './index.css'
@@ -5,11 +6,23 @@ import { createRoot } from "react-dom/client";
 
 import StarRating from "./StarRating";
 
+const Test = () => {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} starts</p>
+    </div>
+  );
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} defaultRating={2} />
     <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating size={24} color="red" />
+    <Test />
   </StrictMode>
 );
