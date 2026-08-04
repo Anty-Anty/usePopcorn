eeb19b24229d90fee2ce84fdc2ff5c2d1a4dea35
Anton <ibeliev@gmail.com>
2026-08-03 17:34:27 -0700

    feat: close MovieDetails with Escape key

--- src/App.jsx
+++ src/App.jsx
@@ -128,6 +128,7 @@ export default function App() {
       return
     }
 
+    handleCloseMovie();
     fetchMovies();
 
     return () => {
@@ -321,60 +322,74 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
   };
 
   useEffect(() => {
-    const getMovieDetails = async () => {
-      setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
-      const data = await res.json();
-      setMovie(data);
-      setIsLoading(false);
-    }
-    getMovieDetails();
-  }, [selectedId])
 
-  useEffect(() => {
-    if (!title) return;
-    document.title = `Movie | ${title}`;
-    return () => {
-      document.title = 'usePopcorn';
-    }
-  }, [title])
-
-  return (
-    <div className="details">
-      {isLoading ? <Loader /> :
-        <>
-          <header>
-            <button className="btn-back" onClick={onCloseMovie}>
-              &larr;
-            </button>
-            <img src={poster} alt={`Poster of ${movie} movie`} />
-            <div className="details-overview">
-              <h2>{title}</h2>
-              <p>{released} &bull; {runtime}</p>
-              <p>{genre}</p>
-              <p><span>⭐</span>{imdbRating} IMDb rating</p>
-            </div>
-          </header>
-
-          <section>
-            <div className="rating">
-
-              {!isWatched ?
-                <>
-                  <StarRating maxRating={10} size={24} onSetRating={setUserRating} />
-                  {userRating > 0 && (<button className="btn-add" onClick={handleAdd}>Add to list</button>)}
-                </>
-                :
-                <p>You rated this movie {isWatched.userRating} <span>⭐</span></p>}
-            </div>
-            <p><em>{plot}</em></p>
-            <p>Starring {actors}</p>
-            <p>Directed by {director}</p>
-          </section>
-        </>
+    const callback = (e) => {
+      if (e.code === 'Escape') {
+        onCloseMovie();
       }
-    </div>
-  )
+    }
+    
+      document.addEventListener('keydown', callback);
+
+  return () => { document.removeEventListener('keydown', callback) }
+
+}, [onCloseMovie]);
+
+useEffect(() => {
+  const getMovieDetails = async () => {
+    setIsLoading(true);
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+    const data = await res.json();
+    setMovie(data);
+    setIsLoading(false);
+  }
+  getMovieDetails();
+}, [selectedId])
+
+useEffect(() => {
+  if (!title) return;
+  document.title = `Movie | ${title}`;
+  return () => {
+    document.title = 'usePopcorn';
+  }
+}, [title])
+
+return (
+  <div className="details">
+    {isLoading ? <Loader /> :
+      <>
+        <header>
+          <button className="btn-back" onClick={onCloseMovie}>
+            &larr;
+          </button>
+          <img src={poster} alt={`Poster of ${movie} movie`} />
+          <div className="details-overview">
+            <h2>{title}</h2>
+            <p>{released} &bull; {runtime}</p>
+            <p>{genre}</p>
+            <p><span>⭐</span>{imdbRating} IMDb rating</p>
+          </div>
+        </header>
+
+        <section>
+          <div className="rating">
+
+            {!isWatched ?
+              <>
+                <StarRating maxRating={10} size={24} onSetRating={setUserRating} />
+                {userRating > 0 && (<button className="btn-add" onClick={handleAdd}>Add to list</button>)}
+              </>
+              :
+              <p>You rated this movie {isWatched.userRating} <span>⭐</span></p>}
+          </div>
+          <p><em>{plot}</em></p>
+          <p>Starring {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+      </>
+    }
+  </div>
+)
 };
 
 const WatchedSummary = ({ watched }) => {
